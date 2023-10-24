aff6f54e87f663d8259ffba288ccf048363dc8a6
Onur Muderrisoglu <onur.muderrisoglu@siemens.com>
2023-10-24 10:57:20 +0300

    Fixed wrong file names.

---
+++ Store.Service/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using Store.Model;
+
+namespace Store.Service
+{
+	public interface IProductService
+	{
+		IEnumerable<Products> GetAllProducts();
+		void DeleteProduct(int id);
+		void AddProduct(Products products);
+	}
+}
---
+++ Store.Service/ProductService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Model;
+
+namespace Store.Service
+{
+	public class ProductService : IProductService
+	{
+		private StoreDbContext _dbContext;
+		public ProductService(StoreDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public void AddProduct(Products products)
+		{
+			_dbContext.Add(products);
+			_dbContext.SaveChangesAsync();
+		}
+
+		public void DeleteProduct(int id)
+		{
+			var entity = GetProductById(id);
+			_dbContext.Set<Products>().Remove(entity);
+			_dbContext.SaveChanges();
+		}
+
+		private Products GetProductById(int id)
+		{
+			return _dbContext.Set<Products>().Find(id);
+		}
+
+		public IEnumerable<Products> GetAllProducts()
+		{
+			return _dbContext.Products.ToList();
+		}
+	}
+}
